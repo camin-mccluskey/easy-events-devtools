d928efff1bdd0484b91a9ac2ec0d7e07d49e9194
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2022-02-22 00:48:02 +0000

    Fix devtools

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,17 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+// kind of pointless
+// var contextMenuItem ={
+//   "id": "easy-event-devtools",
+//   "title": "Easy Events Devtools", /* what appears in the menu */
+//   "contexts": ['page']  /* to make this appear only when user selects something on page */
+// }
+
+// chrome.contextMenus.create(contextMenuItem);
+
+// chrome.contextMenus.onClicked.addListener( (clickData) => {
+//   if(clickData.menuItemId == "easy-event-devtools"){
+//     alert("Should open up devtools")
+//   }
+// })
---
+++ src/pages/Devtools/index.js
@@ -0,0 +1,1 @@
+chrome.devtools.panels.create('Easy Events', 'icon-34.png', 'panel.html');
